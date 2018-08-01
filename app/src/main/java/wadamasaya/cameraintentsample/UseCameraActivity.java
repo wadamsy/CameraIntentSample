a303a5fee725741c6824cd54444f1f4946afdab2
wadamasaya <16010418@anabuki-college.ac.jp>
2018-08-01 11:48:37 +0900

    init

---
+++ app/src/main/java/wadamasaya/cameraintentsample/UseCameraActivity.java
@@ -0,0 +1,13 @@
+package wadamasaya.cameraintentsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UseCameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_use_camera);
+    }
+}
