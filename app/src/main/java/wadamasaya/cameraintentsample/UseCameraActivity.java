2fb4f93503f79eb61fc92a4daeb996df0fdb60a1
wadamasaya <16010418@anabuki-college.ac.jp>
2018-08-01 12:18:15 +0900

    test

--- app/src/main/java/wadamasaya/cameraintentsample/UseCameraActivity.java
+++ app/src/main/java/wadamasaya/cameraintentsample/UseCameraActivity.java
@@ -1,13 +1,66 @@
 package wadamasaya.cameraintentsample;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.INotificationSideChannel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class UseCameraActivity extends AppCompatActivity {
+
+    private Uri _imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_use_camera);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == 200 && resultCode == RESULT_OK){
+            //Bitmap bitmap = data.getParcelableExtra("data");
+            ImageView ivCamera = findViewById(R.id.ivCamera);
+            ivCamera.setImageURI(_imageUri);
+        }
+    }
+    public void onCameraImageClick(View view){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            String [] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            ActivityCompat.requestPermissions(this,permissions,2000);
+            return;
+        }
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+        Date now = new Date(System.currentTimeMillis());
+        String nowStr = dateFormat.format(now);
+        String fileName = "UseCameraActivityPhoto_" + nowStr + "jpg";
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE,fileName);
+        values.put(MediaStore.Images.Media.MIME_TYPE,"image/jpeg");
+        ContentResolver resolver = getContentResolver();
+        _imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, _imageUri);
+
+        startActivityForResult(intent,200);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String[] permissions,int[] grantResults){
+        if (requestCode == 2000 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            ImageView ivCamera = findViewById(R.id.ivCamera);
+            onCameraImageClick(ivCamera);
+        }
     }
 }
